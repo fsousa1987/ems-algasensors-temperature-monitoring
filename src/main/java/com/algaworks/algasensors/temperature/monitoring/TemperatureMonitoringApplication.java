04085f26bdb97907e069e47e3addbfd4529b2cc4
Francisco Sousa <franciscoeds1987@gmail.com>
2026-02-23 18:09:22 -0300

    Adicionando plugins do Gradle

--- src/main/java/com/algaworks/algasensors/temperature/monitoring/TemperatureMonitoringApplication.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/TemperatureMonitoringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TemperatureMonitoringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TemperatureMonitoringApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(TemperatureMonitoringApplication.class, args);
+    }
 
 }
